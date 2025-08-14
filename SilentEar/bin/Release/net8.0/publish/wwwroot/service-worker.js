c8bea3fa2431f0d0d48e32abda1fcfec9f9bbda7
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-08-14 13:27:41 +0545

    headheake

--- SilentEar/bin/Release/net8.0/publish/wwwroot/service-worker.js
+++ SilentEar/bin/Release/net8.0/publish/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: OeAhY0dg */
+/* Manifest version: v5Pov4n7 */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
